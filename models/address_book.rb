19134f4e011c563f9e8d20b821bc51043c26f865
Tory Herman <tory.herman12@gmail.com>
2017-01-02 10:50:01 -0600

    add iterative_search method

--- models/address_book.rb
+++ models/address_book.rb
@@ -49,4 +49,14 @@ class AddressBook
 
     return nil
   end
+
+  def iterative_search(name)
+    entries.each do |entry|
+      if name == entry.name
+        return entry
+      end
+    end
+
+    return nil
+  end
 end
